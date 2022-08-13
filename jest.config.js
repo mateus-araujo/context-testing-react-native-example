e9d7342aad011c2464661ce091724120bd1d1641
Mateus Araujo <rb.mateus.araujo@gmail.com>
2022-08-13 17:29:29 -0300

    add typescript and finish jest configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "react-native",
+  setupFiles: [
+    // "<rootDir>/jest/setup.ts",
+    "./node_modules/react-native-gesture-handler/jestSetup.js",
+  ],
+  globals: {
+    "ts-jest": {
+      babelConfig: true,
+    },
+  }, // this was important
+  moduleNameMapper: {
+    ".+\\.(png)$": "jest-transform-stub",
+    "\\.(svg)$": "<rootDir>/jest/svgMock.js",
+  },
+  transformIgnorePatterns: [], // so was this
+};
---
+++ src/screens/__tests__/Main.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import Main from "../Main";
+
+
+it("renders", () => {
+  // render(<Main />);
+});
